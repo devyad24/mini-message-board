83e8c1c9da5dee83c45b840450fe4f3e3182b3bc
devyad24 <devyadav4211@gmail.com>
2023-08-25 22:53:47 +0530

    Add /home and /new routes

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
+
+
+
+module.exports = router;
